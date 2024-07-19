7da16d5ccde8c998ebfaffc9024b3ae764645b8d
FellipePatrick <fellipe.brito@ifrn.edu.br>
2024-07-19 12:54:29 -0300

    atualização das portas no pin planer

--- Lista1/questao004/Teste.vhd
+++ Lista1/questao004/Teste.vhd
@@ -1,16 +1,16 @@
--- Declaração da entidade
-entity Teste is
-    Port (
-        A : in bit;
-        B : in bit;
-        C : in bit;
-        Y : out bit
-    );
-end Teste;
+	-- Declaração da entidade
+	entity Teste is
+		 Port (
+			  A : in bit;
+			  B : in bit;
+			  C : in bit;
+			  Y : out bit
+		 );
+	end Teste;
 
--- Declaração da arquitetura
-architecture Gate of Teste is
-begin
-    -- Descrição da lógica complexa
-    Y <= A and B and C;
-end Gate;
+	-- Declaração da arquitetura
+	architecture Gate of Teste is
+	begin
+		 -- Descrição da lógica complexa
+		 Y <= (A and B and C) or (A and B and not C) or (A and not B and C) or (not A and B and C);
+	end Gate;
---
+++ Lista1/questao004/Teste.vhd.bak
@@ -0,0 +1,16 @@
+-- Declaração da entidade
+entity Teste is
+    Port (
+        A : in bit;
+        B : in bit;
+        C : in bit;
+        Y : out bit
+    );
+end Teste;
+
+-- Declaração da arquitetura
+architecture Gate of Teste is
+begin
+    -- Descrição da lógica complexa
+    Y <= A and B and C;
+end Gate;
